07ed2b08c92de18e8fda9185f3cf6302bf6f1a28
armando <amarti@mindden.com>
2020-08-01 10:15:56 +0200

    chapter5 -partial

---
+++ chapter5/css3/scripts.js
@@ -0,0 +1,23 @@
+var dialog = document.getElementById('mi-ventana');
+var btn_muestra = document.getElementById('muestra');
+var btn_cierra = document.getElementById('cerrar');
+
+function openCheck(dialog) {
+    if(dialog.open) {
+      console.log('Dialog open');
+    } else {
+      console.log('Dialog closed');
+    }
+  }
+
+ // Update button opens a modal dialog
+ btn_muestra.addEventListener('click', function() {
+    dialog.showModal();
+    openCheck(dialog);
+  });
+
+  // Form cancel button closes the dialog box
+  btn_cierra.addEventListener('click', function() {
+    dialog.close();
+    openCheck(dialog);
+  });
